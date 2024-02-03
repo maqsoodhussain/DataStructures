051a7efbd643f1deb81e41cc49ad61dcff37cb95
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-03 20:22:32 +0530

    edit 2d operations file

--- Arrays/2D_operations.cpp
+++ Arrays/2D_operations.cpp
@@ -1,9 +1,36 @@
 #include<iostream>
 using namespace std;
+ 
+ //Breaking operations in functions 
+
+ //Inseration operation 
+ void inseration(){
+      //creating array
+      int arr[5][5]={0,0},i,n,el,at;
+      int* p;
+
+      cout<<"Enter how many elments you want to enter (<25)\n";
+      cin>>n;
+
+     //pointer p holds first index address
+      p = &arr[0][0];
+ 
+      cout<<" \nEnter "<<n<<" Elements: ";
+      for(i =0; i<n; i++)
+          cin>> *p++;
+
+    p = &arr[0][0];
+    cout<<"\n You enter \n";
+    for(i =0; i<n; i++,p++)
+    cout<<" "<<*p;
+
+    
+ }
 
 
  //driver program
 int main(){
 
+    inseration();
     return 0;
 }
