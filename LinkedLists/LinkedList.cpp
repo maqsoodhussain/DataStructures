54d4489f79bc5196c5cf85e0bd092e66f2dd9680
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-09 16:22:29 +0530

    Representation of linked list

---
+++ LinkedLists/LinkedList.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//structure of linked list
+struct node{
+    int data;
+    node *next;
+
+    //creaing constructure
+    node(int item=0){
+        data = item;
+        next =NULL;
+    }
+};
+
+
+int main(){
+
+}
