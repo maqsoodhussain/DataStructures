27d04f3dfb7be0cd3506c6d53deba8585516768c
MAQSOOD HUSSAIN WANI <83625735+maqsoodhussain@users.noreply.github.com>
2024-02-09 16:36:01 +0530

    Update CreateList.cpp

--- LinkedLists/CreateList.cpp
+++ LinkedLists/CreateList.cpp
@@ -10,12 +10,16 @@ struct list{
 int main(){
     //creating list items
     struct list item0, item1,item2;
+
+    //last node 
     item2.n=3;
     item2.p = NULL;
-    
+
+    //last -1 node
     item1.n =2;
     item1.p= &item2.n;
 
+    //first node 
     item0.n=1;
     item0.p=&item1.n;
 
