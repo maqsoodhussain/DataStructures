2497c8bb2130247b9650de780b84b5d18f198484
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-11 14:09:22 +0530

    Creating linked list

--- LinkedLists/CreatingLinkedList.cpp
+++ LinkedLists/CreatingLinkedList.cpp
@@ -9,6 +9,27 @@ class Node{
 
 int main(){
 
+    //creating three nodes 
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+    
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+//assign data items and next to each node last node next is NULL
+    head->data=1;
+    head->next = second;
+
+    second->data=2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+
 
     return 0;
 }
