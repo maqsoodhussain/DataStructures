d0ff3c6facef75b96351b50d9383466562bc29c8
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-11 13:57:03 +0530

    Representation of linked list

--- LinkedLists/LinkedList.cpp
+++ LinkedLists/LinkedList.cpp
@@ -13,12 +13,13 @@ struct Node{
     }
 };
 
-
+//driver program
 int main(){
     Node *head = new Node(10);
     head->next= new Node(20);
     head->next->next = new Node(30);
    
+   //printing elemments of linked list
    while(head != NULL){
     cout<<head->data<<" ";
     head = head->next;
