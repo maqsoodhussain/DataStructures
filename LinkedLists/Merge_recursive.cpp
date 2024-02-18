9ce4ed5b7f555a995630909f346e358f48c320e5
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-18 20:37:53 +0530

    merge two linked list using recursive method

--- LinkedLists/Merge_recursive.cpp
+++ LinkedLists/Merge_recursive.cpp
@@ -34,7 +34,24 @@ void insertEnd(Node **head, int item){
 
 //function to merge two linked lists
  Node *merge(Node* &head1, Node* &head2){
+      if(head1==NULL){
+        return head2;
+      }
+
+      if(head2==NULL){
+        return head1;
+      }
+
+    Node* result;
+    if(head1->data < head2->data){
+        result = head1;
+        result->next = merge(head1->next, head2);
+    }else{
+        result = head2;
+        result->next = merge(head1, head2->next);
+    }
     
+    return result;
  }
 
 
