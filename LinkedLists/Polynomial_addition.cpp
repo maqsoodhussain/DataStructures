dc3b7caa5c812ed5e4d1c2a228a687445e7eb948
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-18 20:49:32 +0530

    Cresting node structure of polynomial

--- LinkedLists/Polynomial_addition.cpp
+++ LinkedLists/Polynomial_addition.cpp
@@ -14,3 +14,22 @@ Three things a node has :
 
 cpp progrogram to add to polynomial using linked list 
 */
+
+#include<iostream>
+using namespace std;
+
+
+//structure of polynomial node
+struct Node{
+    int coeff; //holds coefficent
+    int pow;  //holds exponent or power
+    Node *next;   //addrss of next node
+
+
+    Node(int c, int p):coeff(c),pow(p),next(NULL){}
+};
+
+Node *addpolly(Node *first, Node *second){
+    Node *result = NULL;
+    Node *last = NULL; // points to last node of result
+}
