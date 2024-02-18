25bb391833f810ddb22f9f5365de21b5dae43d3d
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-18 20:44:32 +0530

    Polynomial using linked list

---
+++ LinkedLists/Polynomial_addition.cpp
@@ -0,0 +1,16 @@
+/* applicaiton of linked list polynomial
+polynomial example= 3x^2+5x
+
+
+add two polynomial 1. 3x^2+5x
+                   2. 6x + 8
+        result =    3x^2 + 11x + 8
+
+
+Three things a node has :
+1.Co-efficient
+2.exponent
+3. link to next node 
+
+cpp progrogram to add to polynomial using linked list 
+*/
