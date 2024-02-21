c183778095fb4a6c7ad0103a0303ecd997bbc78f
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-21 17:31:08 +0530

    dectect loop in linked list program structure only

---
+++ LinkedLists/Detect_loop.cpp
@@ -0,0 +1,27 @@
+//Here i am using Floyds Cycle finding algorithm
+
+#include<iostream>
+using namespace std;
+
+struct  Node{
+        int val;
+        Node *next;
+
+        Node(int x):val(x),next(nullptr){}
+};
+
+void append(Node **head, int data){
+
+}
+
+bool hasloop(Node *head){
+
+}
+
+int main(){
+    
+}
+
+
+
+
