c8de786aa2dbacfc51408be0ae88b98acc7f1c25
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2024-02-24 20:26:05 +0530

    Dectect loop in linked list append function add

--- LinkedLists/Detect_loop.cpp
+++ LinkedLists/Detect_loop.cpp
@@ -10,8 +10,20 @@ struct  Node{
         Node(int x):val(x),next(nullptr){}
 };
 
+//append function 
 void append(Node **head, int data){
+        Node *newNode = new Node(data);
+        if(*head==nullptr){
+                *head = newNode;
+                return;
+        }
 
+        Node *last = *head;
+        while(last->next != nullptr){
+                last = last->next;
+        }
+
+        last->next= newNode;
 }
 
 bool hasloop(Node *head){
